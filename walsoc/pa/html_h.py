cab35669767e037b9ae099ba95dc4891bf06b151
William Martinez Bas <metfar@gmail.com>
2020-10-18 18:24:20 -0300

    conio.h version named colio_h added kb_h added html_h start status md_h starting too metfar.udg is just a test

---
+++ walsoc/pa/html_h.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  html_h.py
+#  
+#  Copyright 2020 William Martinez Bas <metfar@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
+from all_h import *;
+"""
+
+form,input,label,select,textarea,button,fieldset,legend,datalist,output,option,optgroup,h1,h2,h3,h4,h5,h6,p,hr,a,ul,ol,img,div,span
+
+
+"""
+
+
+def main(*argv):
+	echo(APP+LOADED);
+	echo (is_defined("APP"));
+	return(0);
+	
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
